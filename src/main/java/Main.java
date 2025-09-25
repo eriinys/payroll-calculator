7704f401a8182070bec608e432ec37f5ba31d2c8
eriinys <eriinys@duck.com>
2025-09-25 12:32:15 -0500

    fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String name = input.nextLine();
+        System.out.println("Please enter the number of hours you've worked:");
+        double hours = input.nextDouble();
+        System.out.println("Please enter your pay rate: ");
+        double payRate = input.nextDouble();
+
+        double grossPay = hours * payRate;
+        System.out.printf("%s, your gross pay is: $%.2f%n", name, grossPay);
+    }
 }
