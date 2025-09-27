a1623760e4298d2043d67cfafb41035f81112c1e
eriinys <eriinys@duck.com>
2025-09-26 22:25:54 -0500

    added Refactored.java

--- src/main/java/Refactored.java
+++ src/main/java/Refactored.java
@@ -3,41 +3,36 @@ import java.util.Scanner;
 public class Refactored {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String name;
-        double hours;
-        double payRate;
 
-        name = readInputString("Please enter your name:");
-        hours = readInputDouble("Please enter the number of hours you've worked:");
-        payRate = readInputDouble("Please enter your pay rate: ");
+        String name = readInputString(input,"Please enter your name:");
+        double hours = readInputDouble(input,"Please enter the number of hours you've worked:");
+        double payRate = readInputDouble(input,"Please enter your pay rate: ");
 
-        double grossPay;
+        double grossPay = calculateGrossPay(hours, payRate);
 
+        System.out.printf("%s, your gross pay is: $%.2f%n", name, grossPay);
+
+    }
+
+    public static String readInputString(Scanner in, String ask) {
+        System.out.println(ask);
+        return in.nextLine();
+    }
+
+    public static double readInputDouble(Scanner in, String ask) {
+        System.out.println(ask);
+        double value = in.nextDouble();
+        in.nextLine();
+        return value;
+    }
+    public static double calculateGrossPay(double hours, double payRate) {
         if (hours > 40) {
             double normPay = 40 * payRate;
             double overPay = (hours - 40) * payRate * 1.5;
-            grossPay = normPay + overPay;
+            return normPay + overPay;
         } else {
-            grossPay = hours * payRate;
+            return hours * payRate;
         }
-        System.out.printf("%s, your gross pay is: $%.2f%n", name, grossPay);
-    }
-
-    public static String readInputString(String askName) {
-        Scanner input = new Scanner(System.in);
-        System.out.println(askName);
-
-        String answer = input.nextLine();
-        return answer;
-    }
-
-    public static double readInputDouble(String ask) {
-        Scanner input = new Scanner(System.in);
-        System.out.println(ask);
-
-        String answer2 = input.nextLine();
-        double hours = Double.parseDouble(answer2);
-        return hours;
     }
 
 }
